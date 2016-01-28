67c757f7fa64ca1f0d0530eff7e663a80cab9f5f
Hongxingdu <dhxjoyce@163.com>
2016-01-29 11:41:16 +1300

    the first version

---
+++ __openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+#                                                                            #
+#   OpenERP Module                                                           #
+#   Copyright (C) 2013 Author <email@email.fr>                               #
+#                                                                            #
+#   This program is free software: you can redistribute it and/or modify     #
+#   it under the terms of the GNU Affero General Public License as           #
+#   published by the Free Software Foundation, either version 3 of the       #
+#   License, or (at your option) any later version.                          #
+#                                                                            #
+#   This program is distributed in the hope that it will be useful,          #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#   GNU Affero General Public License for more details.                      #
+#                                                                            #
+#   You should have received a copy of the GNU Affero General Public License #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.    #
+#                                                                            #
+
+{
+    "name": "WebSite Event Table",
+    "version": "1.0",
+    "depends": ["website_event_sale","event_sale"],
+    "author": "sum1201@163.com",
+    "category": "",
+    "description": """
+    """,
+    "data":[
+        'views/event_view.xml',
+        'views/website_event.xml'
+    ],
+    "css":[
+        'static/src/css/widget_event.css'
+    ],
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': True,
+    #    'certificate': '',
+}
---
+++ models/event.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+import openerp.addons.decimal_precision as dp
+import datetime
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class event_event(models.Model):
+    _inherit = 'event.event'
+
+    @api.one
+    @api.depends('date_begin','date_end')
+    def _cal_days(self):
+        result=[]
+        _logger.info("cal.......")
+        _logger.info("date_begin:"+ str(self.date_begin))
+        date_begin=datetime.datetime.strptime(self.date_begin,'%Y-%m-%d %H:%M:%S')
+        date_end=datetime.datetime.strptime(self.date_end,'%Y-%m-%d %H:%M:%S')
+        _logger.info("result:"+str((date_end-date_begin).seconds))
+        days=(date_end-date_begin).days
+        hour=(date_end-date_begin).seconds/3600
+        if days>0:
+            self.duration_day=str(days)+"天"+str(hour)+'小时'
+        else:
+            self.duration_day=str(hour)+'小时'
+
+    @api.one
+    @api.depends('event_ticket_ids')
+    def _cal_price(self):
+        vals=[]
+        _logger.info("***************")
+        if self.event_ticket_ids:
+            for l in self.event_ticket_ids:
+                vals.append(l.price)
+
+
+        if len(vals)>0:
+            self.price= vals[vals.index(min(vals))]
+        else:
+            self.price= 0
+
+    duration_day=fields.Char(compute='_cal_days',string=u'天数')
+    price =fields.Float(compute='_cal_price',string="价格")
+
+
+
